a4624689ec6cc159ee7330c332930dca9ca85077
pranith-web <pranithk2005@gmail.com>
2024-09-08 01:36:06 +0530

    Create Winners.jsx

---
+++ Winners.jsx
@@ -0,0 +1,22 @@
+function Winner() {
+    return ( 
+
+  <div class="container">
+    <div className="rank2">
+        <h3>#2</h3> <br/>
+        <h3>Jackson</h3>
+        </div>
+    <div className="rank1">
+    <h3>#1</h3> <br/>
+    <h3>Eiden</h3>
+    </div>
+    <div className="rank3">
+    <h3>#3</h3> <br/>
+    <h3>Ariana</h3>    
+    </div>
+  </div>
+</div>
+     );
+}
+
+export default Winner;
