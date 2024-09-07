8e00bc767380e224437fe60b4d2e656e85d125b5
pranith-web <pranithk2005@gmail.com>
2024-09-08 01:35:26 +0530

    Create Runners.jsx

---
+++ Runners.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+function Runners() {
+  return (
+    <div>
+      <div className="rankings">
+        <div className="ranking-ranks">
+          <div className="margdiv"><h5 className="rank">#3</h5></div>
+          <div className="margdiv"><h5 className="rank">#4</h5></div>
+          <div className="margdiv"><h5 className="rank">#5</h5></div>
+          <div className="margdiv"><h5 className="rank">#6</h5></div>
+          <div ><h5 className="rank">#7</h5></div>
+        </div>
+        <div className="ranking-names">
+          <div className="margdiv"><h5 className="names">Sebastian</h5></div>
+          <div className="margdiv"><h5 className="names">Jason</h5></div>
+          <div className="margdiv"><h5 className="names">Natalie</h5></div>
+          <div className="margdiv"><h5 className="names">Serenity</h5></div>
+          <div><h5 className="names">Hannah</h5></div>
+        </div>
+        <div className="ranking-points">
+          <div className="margdiv"><h5 className="points">1124</h5></div>
+          <div className="margdiv"><h5 className="points"> 875</h5></div>
+          <div className="margdiv"><h5 className="points">774</h5></div>
+          <div className="margdiv"><h5 className="points">723</h5></div>
+          <div><h5 className="points">559</h5></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Runners;
